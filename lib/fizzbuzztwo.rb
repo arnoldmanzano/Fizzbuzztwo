bf104d2e6e382a0956a2f56d1bcdf0aa99bd9de5
arnoldmanzano <arnold.manzano@gmail.com>
2016-01-15 15:31:43 +0000

    Refactor fizzbuzztwo.rb

--- lib/fizzbuzztwo.rb
+++ lib/fizzbuzztwo.rb
@@ -1,22 +1,15 @@
 def fizzbuzz number
 
   if number == 0
-    return 0
+    0
+  elsif number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  else
+    number
   end
 
-  if number % 3 == 0 && number % 5 == 0
-    return 'fizzbuzz'
-  end
-
-  if number % 3 == 0
-    return 'fizz'
-  end
-
-  if number % 5 == 0
-    return 'buzz'
-  end
-
-  if !(number % 3 == 0 && number % 5 == 0)
-    return number
-  end
 end

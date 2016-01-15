a3f52901fe62f5db8abf3a17d1f47bbad863d3b9
esbaddeley <esbaddeley@gmail.com>
2016-01-15 15:15:15 +0000

    Written another test and made both tests pass

--- lib/fizzbuzztwo.rb
+++ lib/fizzbuzztwo.rb
@@ -1,5 +1,11 @@
 def fizzbuzz number
 
-  "fizz"
+  if number % 3 == 0
+    return 'fizz'
+  end
+
+  if number % 5 == 0
+    return 'buzz'
+  end
 
 end
--- spec/fizzbuzztwo_spec.rb
+++ spec/fizzbuzztwo_spec.rb
@@ -4,4 +4,9 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed three' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed five' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
 end
